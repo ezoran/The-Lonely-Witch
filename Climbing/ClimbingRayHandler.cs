817f98a184d34fe53dab6def13505eee19ab7903
ezoran <ezoran@uccs.edu>
2018-06-11 11:25:24 -0600

    added climb mechanic code

---
+++ Climbing/ClimbingRayHandler.cs
@@ -0,0 +1,134 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ClimbingRayHandler : MonoBehaviour {
+
+    /*
+     * This class is used to check for climbable surfaces, sending out a ray in the forward direction
+     * to look for surfaces that are on the "Climbable" Layer.
+     * 
+     * Attached to an empty object parented to the player object
+     */
+    public GameObject player;
+    private Text actionText;
+
+
+
+
+    Vector3 fwd;
+    LayerMask mask;
+    LayerMask inverse;
+
+    RaycastHit wallHit;
+
+    public Transform leftEdgeRay;
+    public Transform rightEdgeRay;
+    public Transform topEdgeRay;
+
+    public float rayDistance = 10.0f;
+
+    void Start()
+    {
+       
+        mask = LayerMask.GetMask("Climbable");
+        inverse = LayerMask.GetMask("ClimbableInverse");
+
+        actionText = GameObject.FindGameObjectWithTag("ClimbText").GetComponent<UnityEngine.UI.Text>();
+
+    }
+
+    void mainRayDirectionSet()
+    {
+        Vector3 mainRayForward = transform.TransformDirection(Vector3.forward);
+        Vector3 mainRayBackward = transform.TransformDirection(Vector3.back);
+        Vector3 mainRayLeft = transform.TransformDirection(Vector3.left);
+        Vector3 mainRayRight = transform.TransformDirection(Vector3.right);
+
+        if ((Physics.Raycast(transform.position, mainRayForward, out wallHit, rayDistance, mask)))
+        {
+            //forward ray
+            fwd = mainRayForward;
+          //  Debug.Log("Forward Side Hit");
+        }
+        else if((Physics.Raycast(transform.position, mainRayBackward, out wallHit, rayDistance, mask)))
+        {
+            fwd = mainRayBackward;
+         //   Debug.Log("Backward Side Hit");
+
+        }
+        else if ((Physics.Raycast(transform.position, mainRayLeft, out wallHit, rayDistance, mask)))
+        {
+            fwd = mainRayLeft;
+         //   Debug.Log("Left Side Hit");
+
+        }
+        else if ((Physics.Raycast(transform.position, mainRayRight, out wallHit, rayDistance, mask)))
+        {
+            fwd = mainRayRight;
+         //   Debug.Log("Right Side Hit");
+
+        }
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate () {
+
+        mainRayDirectionSet();
+
+        CharacterController controller = this.transform.parent.GetComponent<CharacterController>();
+
+        //  Debug.Log("Forward " + fwd);
+
+
+
+        // Debug.DrawRay(transform.position, fwd * rayDistance, Color.red);
+        Debug.DrawRay(topEdgeRay.position, fwd * rayDistance, Color.green);
+
+        if ((Physics.Raycast(transform.position, fwd, out wallHit, rayDistance, mask)))
+        {
+            actionText.text = "Press RB To Climb";
+        }
+        else
+        {
+            actionText.text = "";
+        }
+
+
+        if ((Physics.Raycast(transform.position, fwd, out wallHit,  rayDistance, mask)) && Input.GetButton("RB Button") && controller.isGrounded) //main climb ray checker
+        {
+            actionText.text = "";
+
+            HoldValues.climbMovement = true; //ensure that horizontal plane movement is turned off
+            HoldValues.currentClimbingObject = wallHit.collider.transform; //set static variable to current climbing object
+
+            //The following if-statement rotates the player to face each desired side of the object
+            if(transform.parent.rotation != HoldValues.currentClimbingObject.rotation && !HoldValues.isParallel)
+            {
+                Vector3 roundedFWD = new Vector3((Mathf.Round(fwd.x)), Mathf.Round(fwd.y), Mathf.Round(fwd.z));
+                Debug.Log("Forward Rounded " + roundedFWD);
+                transform.parent.rotation = Quaternion.LookRotation(roundedFWD, Vector3.up);
+                HoldValues.isParallel = true;
+            }
+    
+            if (!Physics.Raycast(topEdgeRay.position, fwd, rayDistance, mask) && !Physics.Raycast(topEdgeRay.position, fwd, rayDistance, inverse)) //checks to see if player has hit top edge
+            {
+                HoldValues.topEdgeHit = true;
+            }
+         
+
+        }
+        else
+        {
+            HoldValues.climbMovement = false;
+            HoldValues.horizontalMovement = true;
+            HoldValues.currentClimbingObject = null;
+            HoldValues.isParallel = false;
+
+        }
+    }
+
+
+}
---
+++ Climbing/PlayerClimb.cs
@@ -0,0 +1,103 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerClimb : MonoBehaviour {
+
+    private Animator anim;
+
+    public float climbingSpeed;
+    public Vector3 distanceToTop = new Vector3(0, 1, 1);
+    float controllerDrag = 0.007f;
+
+    private float saveClimbingSpeed;
+
+    float vert;
+    float hori;
+
+    // Use this for initialization
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        saveClimbingSpeed = climbingSpeed;
+
+    }
+
+    
+    // Update is called once per frame
+    void Update()
+    {
+        // Debug.Log("isClimbing: " + anim.GetBool("isClimbing"));
+
+        //check if climb check has been set
+        //Debug.Log("climbingSpeed: " + climbingSpeed);
+
+        ClimbingAnimationHandler();
+
+        if (HoldValues.climbMovement == true)
+        {
+            HoldValues.horizontalMovement = false; //turn normal movement off
+
+
+            MovePlayer();
+
+            if(HoldValues.topEdgeHit)
+            {
+                ClimbToTop();
+            }
+        }
+        else
+        {
+            anim.SetBool("isClimbing", false);
+
+        }
+    }
+
+    void MovePlayer()
+    {
+        vert = Mathf.Round(10 * (Input.GetAxisRaw("Joy LY")));
+        hori = Mathf.Round(10 * (-Input.GetAxisRaw("Joy LX")));
+
+       
+        if (Input.GetAxis("Joy LY") < -controllerDrag || Input.GetAxis("Joy LY") > controllerDrag || Input.GetAxis("Joy LX") < -controllerDrag || Input.GetAxis("Joy LX") > controllerDrag)
+        {
+            if (vert == 0)
+            {
+              //  Debug.Log("Climbing horizontally");
+                transform.Translate(Vector3.left * (hori / 10) * climbingSpeed * Time.deltaTime);
+            }
+            else if (hori == 0)
+            {
+              //  Debug.Log("Climbing vertically");
+                transform.Translate(Vector3.up * (vert / 10) * climbingSpeed * Time.deltaTime);
+            }
+        }
+      
+
+        //Debug.Log("Vertical " + vert);
+       Debug.Log("Horizontal " + hori);
+
+       
+
+      
+    }
+    
+
+
+    void ClimbToTop()
+    {
+        //code to move player to top of object once top edge has been hit
+        Debug.Log("Climb to edge");
+        
+        transform.Translate(distanceToTop);
+        HoldValues.topEdgeHit = false;
+    }
+
+    void ClimbingAnimationHandler()
+    {
+        anim.SetBool("isClimbing", true);
+        anim.SetFloat("InputX", Input.GetAxis("Joy LX"));
+        anim.SetFloat("InputY", Input.GetAxis("Joy LY"));
+    }
+
+}
